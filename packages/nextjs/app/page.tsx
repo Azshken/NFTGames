562760113e3b67d0e8ec2e97e086dd56ee78798d
Azshken <mardivh@gmail.com>
2026-03-16 16:08:50 +0100

    SoulKey img fix

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -661,7 +661,7 @@ const Home: NextPage = () => {
         <h1 className="text-center">
           <span className="block text-4xl font-bold mb-8">SoulKey</span>
         </h1>
-        <p className="subtitle text-center font-medium">Virtual Game Keys</p>
+        <p className="subtitle flex items-center justify-center font-medium">Virtual Game Keys</p>
 
         {/* Contract address */}
         <div className="flex justify-center items-center flex-col mb-8">
@@ -683,7 +683,7 @@ const Home: NextPage = () => {
                 {selectedProduct.image_cid && (
                   <figure className="px-6 pt-6">
                     <Image
-                      src={`https://${process.env.NEXT_PUBLIC_PINATA_GATEWAY}/ipfs/${selectedProduct.image_cid}`}
+                      src={`${selectedProduct.image_cid}`}
                       alt={selectedProduct.name}
                       width={384}
                       height={192}
