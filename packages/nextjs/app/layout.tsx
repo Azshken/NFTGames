a34bda999029b0a9b52ad841e1ad45527bdb2c8a
Azshken <mardivh@gmail.com>
2026-03-24 14:08:43 +0100

    Wagmi fix

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -4,17 +4,23 @@ import { Providers } from "~~/components/ProvidersClient";
 import { Header } from "~~/components/HeaderClient";
 import { Footer } from "~~/components/Footer";
 import { Toaster } from "sonner";
+import { cookies } from "next/headers";
+import { cookieToInitialState } from "wagmi";
+import { config } from "~~/components/ProvidersClient";
 
 export const metadata = {
   title: "Soulkey Store",
   description: "Mint verifiable game keys as virtual game cards",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode
+}) {
+  const initialState = cookieToInitialState(config, (await cookies()).toString());
+
   return (
     <html lang="en">
       <body>
-        <Providers>
+        <Providers initialState={initialState}>
           <Header />
           <Toaster position="bottom-right" theme="dark" richColors />
           {children}
--- packages/nextjs/components/ProvidersClient.tsx
+++ packages/nextjs/components/ProvidersClient.tsx
@@ -3,8 +3,9 @@ import { darkTheme, getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rai
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { sepolia } from "viem/chains";
 import { cookieStorage, createStorage, WagmiProvider } from "wagmi";
+import { State } from "wagmi";
 
-const config = getDefaultConfig({
+export const config = getDefaultConfig({
   appName: "SoulKey Store",
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
   chains: [sepolia],
@@ -14,7 +15,10 @@ const config = getDefaultConfig({
 
 const queryClient = new QueryClient();
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children, initialState }: {
+  children: React.ReactNode;
+  initialState?: State;
+ }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
