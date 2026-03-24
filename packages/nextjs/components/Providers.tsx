3a918a6ada1804874295fcba5bb8ed70f8d75c33
Azshken <mardivh@gmail.com>
2026-03-24 14:59:08 +0100

    clean up

--- packages/nextjs/components/Providers.tsx
+++ packages/nextjs/components/Providers.tsx
@@ -1,2 +1,26 @@
-export const dynamic = "force-dynamic";
-export { Providers as default } from "./ProvidersClient";
+"use client";
+import { darkTheme, getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { sepolia } from "viem/chains";
+import { WagmiProvider } from "wagmi";
+
+const config = getDefaultConfig({
+  appName: "SoulKey Store",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
+  chains: [sepolia],
+  ssr: false,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme({ accentColor: "#10b981" })}>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
